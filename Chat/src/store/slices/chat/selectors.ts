c1fdb70f550293e7624cca242df28f87368a10fe
Clearenough <smogida1337@gmail.com>
2023-12-04 13:12:53 +0300

    refactor: move selectors to separate folders

--- Chat/src/store/slices/chat/selectors.ts
+++ Chat/src/store/slices/chat/selectors.ts
@@ -1,19 +1,19 @@
-import {createSelector} from '@reduxjs/toolkit';
-import {RootState} from '../../store';
-import {selectUser} from '../user/selectors';
+// import {createSelector} from '@reduxjs/toolkit';
+// import {RootState} from '../../store';
+// import {selectUser} from '../user/selectors';
 
-const selectChats = (state: RootState) => state.chat.chats;
+// const selectChats = (state: RootState) => state.chat.chats;
 
-export const selectCurrentChat = createSelector(
-  [selectChats, selectUser],
-  (chats, user) => {
-    return chats.find(chat => {
-      return (
-        chat.members.includes(user._id) && chat.members.includes(secondUserId)
-      );
-    });
-  },
-);
+// export const selectCurrentChat = createSelector(
+//   [selectChats, selectUser],
+//   (chats, user) => {
+//     return chats.find(chat => {
+//       return (
+//         chat.members.includes(user._id) && chat.members.includes(secondUserId)
+//       );
+//     });
+//   },
+// );
 
 // const currentChat = useAppSelector(state => {
 //   return state.chat.chats.find(chat => {
